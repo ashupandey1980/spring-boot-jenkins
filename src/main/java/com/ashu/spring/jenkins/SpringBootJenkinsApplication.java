f7a3a53b3e2a5cd152d212771aeca6dcc0bd7c6a
Ashutosh Pandey <apandey@digitalharbor.com>
2022-03-31 19:43:51 +0530

    first commit

---
+++ src/main/java/com/ashu/spring/jenkins/SpringBootJenkinsApplication.java
@@ -0,0 +1,26 @@
+package com.ashu.spring.jenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringBootJenkinsApplication {
+
+	public static Logger logger= LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+
+	@PostConstruct
+	public void init(){
+		logger.info("application started..");
+	}
+
+	public static void main(String[] args) {
+		logger.info("application executed..");
+		SpringApplication.run(SpringBootJenkinsApplication.class, args);
+
+	}
+
+}
